43e33a4e95d3333b5b6516bab34b364a3dbb1a29
julesjacobs <julesjacobs@gmail.com>
2023-11-10 22:26:33 -0500

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -69,15 +69,21 @@ check_bisim() {
     local file2=$2
     local expected_output=$3
 
-    # Run frenetic dump bisim and capture the output
-    local output=$(frenetic dump bisim "$file1" "$file2")
+    # Run frenetic dump bisim and capture the output, time and memory usage
+    local cmd_output
+    cmd_output=$(/usr/bin/time -f "\nTime: %E\nMemory: %MKB" frenetic dump bisim "$file1" "$file2" 2>&1)
 
-    # Compare the output with the expected output and print a single line result
-    if [ "$output" == "$expected_output" ]; then
-        echo -e "\e[32mSuccess for $file1, $file2 - Expected: $expected_output\e[0m"
+    # Extract the output and the time/memory statistics
+    local frenetic_output=$(echo "$cmd_output" | head -n -2)
+    local time_memory=$(echo "$cmd_output" | tail -n 2)
+
+    # Compare the frenetic output with the expected output and print a single line result
+    if [ "$frenetic_output" == "$expected_output" ]; then
+        # Print success message in green along with time and memory usage
+        echo -e "\e[32mSuccess for $file1, $file2 - Expected: $expected_output$time_memory\e[0m"
     else
-        # Print failure message in red to stderr
-        echo -e "\e[31mFailure for $file1, $file2 - Expected: $expected_output, Got: $output\e[0m" >&2
+        # Print failure message in red to stderr along with time and memory usage
+        echo -e "\e[31mFailure for $file1, $file2 - Expected: $expected_output, Got: $frenetic_output$time_memory\e[0m" >&2
     fi
 }
 
