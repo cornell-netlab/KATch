f71eddf32fdcf7834017ebb03e4a5b2a702c691d
Mark Moeller <moeller@cs.cornell.edu>
2023-11-13 18:24:37 -0500

    linear reachability queries

--- benchmarks/topo-zoo/rch.py
+++ benchmarks/topo-zoo/rch.py
@@ -2,23 +2,30 @@
 import sys
 
 if len(sys.argv) < 2:
-    print("usage: gen-lin.py n")
+    print("usage: gen-lin.py ")
     sys.exit(1)
 
-n = int(sys.argv[1])
+n = int(sys.argv[1])-1 # subtract 1 because NKPL ranges are inclusive
 
+print(f'net = (main⋅(top) ⋅ δ)⋆')
+print(f'all = rangesum @sw 0..{n}')
+print(f'for i ∈ 0..{n} do check exists @pt (exists @dst (forward (@sw=i ⋅ net))) ≡ all')
+
+
+
+
+
+# Notes for big cross product method
+"""
 bigstart = "@st=N0⋅@sw=N0"
 sum1 = "@sw=N0"
 sum2 = "@st=N0"
-
-# print backward ((exists @pt (exists @dst (forward bigstart⋅(main⋅top⋅δ)⋆))) ^ bigfinish)
 
 for i in range(1,n):
     bigstart += f' ∪ @st=N{i}⋅@sw=N{i}'
     sum1 += f' ∪ @sw=N{i}'
     sum2 += f' ∪ @st=N{i}'
 
-print(f'bigstart = {bigstart}')
-print(f'sum1 = {sum1}')
-print(f'sum2 = {sum2}')
-print("check forward (exists @pt exists @dst forward bigstart⋅(main⋅top⋅δ)⋆) ≡ sum1⋅sum2")
+then
+check forward (exists @pt (exists @dst (forward bigstart⋅(main⋅top⋅δ)⋆))) ≡ sum1⋅sum1
+"""
--- run-jar.sh
+++ run-jar.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-time java -Xmx14g -jar target/scala-3.3.1/KATch-assembly-0.1.0-SNAPSHOT.jar run $1
+sbt assembly
+time java -Xmx15g -jar target/scala-3.3.1/KATch-assembly-0.1.0-SNAPSHOT.jar run $1
