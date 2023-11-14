a9bb4100f61e22559b01db491ed2a45c81f888af
Mark Moeller <moeller@cs.cornell.edu>
2023-11-14 17:04:08 -0500

    org tweaks

--- run-jar.sh
+++ run-jar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sbt assembly
-time java -Xmx15g -jar target/scala-3.3.1/KATch-assembly-0.1.0-SNAPSHOT.jar run $1
+time java -Xmx128g -jar target/scala-3.3.1/KATch-assembly-0.1.0-SNAPSHOT.jar $1 $2
