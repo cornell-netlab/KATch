fc1613525f7f1cd54c4b4c5684caf70c42ea90ed
julesjacobs <julesjacobs@gmail.com>
2023-11-14 19:14:33 -0500

    Update texgen.py

--- texgen.py
+++ texgen.py
@@ -5,13 +5,51 @@ import os
 # Read the CSV files
 performance_data = pd.read_csv('benchresults/comparison.csv')
 
+import re
+
+def get_file_size(filepath):
+    """
+    Recursively computes the size of a file and all files it imports.
+    The import paths are relative to the file itself.
+    """
+    total_size = 0
+    files_processed = set()
+
+    def process_file(file):
+        if file in files_processed:
+            return 0
+        files_processed.add(file)
+
+        try:
+            with open(file, 'r') as f:
+                contents = f.read()
+            size = os.path.getsize(file) / 1000.0
+        except IOError:
+            print(f"Error reading file: {file}")
+            return 0
+
+        # Add the size of the current file
+        nonlocal total_size
+        total_size += size
+
+        # Find and process all imported files
+        for line in contents.split('\n'):
+            match = re.match(r'import "(.+?)"', line)
+            if match:
+                # Construct path relative to the current file
+                imported_file = os.path.join(os.path.dirname(file), match.group(1))
+                process_file(imported_file)
+
+    process_file(filepath)
+    return total_size
+
 # Make a benchmark_data dataframe with the following structure:
 # file, size
 benchmark_data = []
 # Look up all the file sizes for files that occur in performance_data
 for file in performance_data['file'].unique():
     # Get the file size from disk
-    size = os.path.getsize(file)
+    size = get_file_size(file)
 
     # Add the file and size to the benchmark_data dataframe
     benchmark_data.append({'file': file, 'size': size})
@@ -53,9 +91,19 @@ print(column_order)
 # Reorder the columns
 pivot_table = pivot_table.reindex(column_order, axis=1)
 pivot_table.reset_index(inplace=True)
+# Sort the table by size
+pivot_table.sort_values(by=['size'], inplace=True)
+# Round the size column to 1 decimal place
+pivot_table['size'] = pivot_table['size'].round(1)
+# Round the time columns to 2 decimal places
+for sys in systems:
+    pivot_table[sys] = pivot_table[sys].round(2)
+
+# Make table headers read File, Size (KB), KATch, Frenetic
+pivot_table.rename(columns={'file': 'File', 'size': 'Size (KB)', 'katch': 'KATch (s)', 'frenetic': 'Frenetic (s)'}, inplace=True)
 
 # Convert to LaTeX table
-latex_table = pivot_table.to_latex(index=False, na_rep='n/a')
+latex_table = pivot_table.to_latex(index=False, na_rep='n/a', float_format='%.2f')
 
 # Save the LaTeX table to a .tex file
 with open('benchresults/comparison_table.tex', 'w') as f:
@@ -103,7 +151,7 @@ plt.savefig('benchresults/katch_vs_frenetic_scatterplot.pdf', format='pdf', bbox
 plt.figure(figsize=(5, 5))
 sns.scatterplot(x='size', y='time', hue='system', data=merged_data)
 plt.title('Time vs Size for Each System')
-plt.xlabel('Size (MB)')
+plt.xlabel('Size (KB)')
 plt.ylabel('Time (s)')
 plt.legend()
 plt.savefig('benchresults/time_vs_size_combined.pdf', format='pdf', bbox_inches='tight')
