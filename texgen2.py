8f00ebdf05e782b64cb6c5ecf1664de6724d696c
julesjacobs <julesjacobs@gmail.com>
2023-11-16 18:53:26 -0500

    Texgen

--- texgen2.py
+++ texgen2.py
@@ -3,47 +3,41 @@ import seaborn as sns
 import pandas as pd
 import re
 import os
+import sys
 
-def get_file_size(filepath):
-    """
-    Recursively computes the size of a file and all files it imports.
-    The import paths are relative to the file itself.
-    """
+def get_file_size(file):
+    try:
+        with open(file, 'r') as f:
+            contents = f.read()
+    except IOError:
+        print(f"\033[91mError reading file: {file}")
+        return 0
+
     total_size = 0
-    files_processed = set()
+    # if combinatorial in the name, include the file size itself
+    if "inc" in file or "flip" in file or "nondet" in file:
+        print(f"combinatorial in {file}")
+        total_size += os.path.getsize(file) / 1000.0
+        return total_size
+        print(f"total size: {total_size}")
 
-    def process_file(file):
-        if file in files_processed:
-            return 0
-        files_processed.add(file)
+    # Find and process all imported files
+    for line in contents.split('\n'):
+        match = re.match(r'import "(.+?)"', line)
+        if match:
+            # Construct path relative to the current file
+            imported_file = os.path.join(os.path.dirname(file), match.group(1))
+            size = os.path.getsize(imported_file) / 1000.0
+            total_size += size
 
-        try:
-            with open(file, 'r') as f:
-                contents = f.read()
-            size = os.path.getsize(file) / 1000.0
-        except IOError:
-            print(f"\033[91mError reading file: {file}")
-            return 0
-
-        # Add the size of the current file
-        nonlocal total_size
-        total_size += size
-
-        # Find and process all imported files
-        for line in contents.split('\n'):
-            match = re.match(r'import "(.+?)"', line)
-            if match:
-                # Construct path relative to the current file
-                imported_file = os.path.join(os.path.dirname(file), match.group(1))
-                process_file(imported_file)
-
-    process_file(filepath)
     return total_size
 
 def group_fn(path):
     if re.search("small|medium|large", path):
         return "Topology Zoo"
     if re.search("linear-reach", path):
+        print("ERRROR")
+        sys.exit(1)
         return "Linear reachability"
     if re.search("topo-zoo", path):
         return "Full reachability"
@@ -69,17 +63,27 @@ types = []
 times = []
 timeouts = []
 sizes = []
+algs = []
 
 # Process each line
 for line in data.split('\n'):
     if not line: continue
     system, path, time = line.split(',')
+    size = get_file_size(path)
+    # If "linear-reachability" is in the path, replace system with "katch (linear)", and replace "linear-reachability" with "naive-reachability"
+    alg = "naive"
+    if "linear-reachability" in path:
+        system = "katch"
+        path = path.replace("linear-reachability", "naive-reachability")
+        alg = "linear"
     path_parts = path.split('/')
     # group = path_parts[2]
     # path_parts[-1] on on "-"" and "_" using regex
     name_type = re.split(r'[-_]', path_parts[-1])
     name = name_type[0].split('.')[0]
     type = name_type[-1].split('.')[0]
+    if alg == "linear":
+        type = "reachability"
     if type not in ['slicing', 'reachability', 'unreachability']:
         type = 'none'
 
@@ -98,7 +102,11 @@ for line in data.split('\n'):
     types.append(type)
     times.append(time)
     timeouts.append(timeout)
-    sizes.append(get_file_size(path))
+    sizes.append(size)
+    algs.append(alg)
+    # if alg is linear, print all fields
+    # if alg == "linear":
+        # print(f"{system}, {group_fn(path)}, {name}, {type}, {size}, {time}, {timeout}, {alg}")
 
 # Create DataFrame
 df = pd.DataFrame({
@@ -108,23 +116,29 @@ df = pd.DataFrame({
     'size': sizes,
     'system': systems,
     'time': times,
-    'timeout': timeouts
+    'timeout': timeouts,
+    'alg': algs
 })
+
+# write dataframe to csv out.csv
+df.to_csv('out.csv')
 
 # Display the DataFrame
 
 # Filter data for 'frenetic' and 'katch'
 frenetic_data = df[df['system'] == 'frenetic']
 katch_data = df[df['system'] == 'katch']
+print(katch_data['alg'].unique())
 
 # Group by 'name', 'type', and 'group', then calculate the mean time
-frenetic_avg = frenetic_data.groupby(['name', 'type', 'group', 'size'])['time'].mean().reset_index()
-katch_avg = katch_data.groupby(['name', 'type', 'group', 'size'])['time'].mean().reset_index()
+frenetic_avg = frenetic_data.groupby(['name', 'type', 'group', 'size', 'alg'])['time'].mean().reset_index()
+katch_avg = katch_data.groupby(['name', 'type', 'group', 'size', 'alg'])['time'].mean().reset_index()
+print(katch_avg['alg'].unique())
 
-# Merge the averaged data
 merged_data = pd.merge(frenetic_avg, katch_avg, on=['name', 'type', 'group', 'size'], suffixes=('_frenetic', '_katch'))
-print(merged_data)
 
+print(merged_data['alg_katch'].unique())
+# exit()
 # Plotting
 
 sizes = {'': (3,3), '_wide': (10, 4)}
@@ -132,6 +146,7 @@ system_color_symbols = {
     'frenetic': ('#1f77b4', 'o'),
     'frenetic (timeout)': ('#000', 'X'),
     'katch': ('#ff7f0e', 'o'),
+    'katch (linear)': ('#2ca02c', 'o'),
 }
 
 palette = {system: color for system, (color, marker) in system_color_symbols.items()}
@@ -158,15 +173,17 @@ for group in merged_data['group'].unique():
     frenetic_data = group_data[['size', 'time_frenetic']].copy()
     frenetic_data.rename(columns={'time_frenetic': 'time'}, inplace=True)
     frenetic_data['system'] = 'frenetic'
-    katch_data = group_data[['size', 'time_katch']].copy()
+    katch_data = group_data[['size', 'time_katch', 'alg_katch']].copy()
     katch_data.rename(columns={'time_katch': 'time'}, inplace=True)
     katch_data['system'] = 'katch'
+    print(katch_data['alg_katch'].unique())
 
     # combine frenetic and katch data
     group_data_kf = pd.concat([frenetic_data, katch_data])
 
     # Add a separate system "frenetic (timeout)" for the timeouts
     group_data_kf['system'] = group_data_kf.apply(lambda row: f"{row['system']} (timeout)" if row['time'] == 300 else row['system'], axis=1)
+    group_data_kf['system'] = group_data_kf.apply(lambda row: f"{row['system']} (linear)" if row['alg_katch'] == 'linear' else row['system'], axis=1)
 
     for sizename, size in sizes.items():
         plt.figure(figsize=size)
@@ -195,7 +212,7 @@ for group in merged_data['group'].unique():
 
     final_table_df['sort_key'] = final_table_df.apply(lambda row: row['name'].startswith("ft"), axis=1)
 
-    print(final_table_df)
+    # print(final_table_df)
 
     # Sorting by 'size'
     final_table_df = final_table_df.sort_values(by=['sort_key', 'size']).drop('sort_key', axis=1)
@@ -206,7 +223,7 @@ for group in merged_data['group'].unique():
     # Calculate speedup, round to integer
     final_table_df['Speedup'] = (final_table_df['Frenetic (s)'] / final_table_df['KATch (s)']).round().astype(int)
 
-    print(final_table_df)
+    # print(final_table_df)
 
     # Convert to LaTeX format
     # latex_table = final_table_df.to_latex(index=False)
