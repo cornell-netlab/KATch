180034c5d1fe7d442d2339370b6fe2c9f429c679
Mark Moeller <moeller@cs.cornell.edu>
2024-02-29 16:10:29 -0500

    mistake in run-paper-experiments

--- scripts/run-paper-experiments.sh
+++ scripts/run-paper-experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./katch compare nkpl/fig09/naive-reachability
 ./katch run nkpl/fig09/linear-reachability
-./katch compare nkpl/fig10/*-first*
-./katch compare nkpl/fig11
+./katch compare nkpl/fig10
+./katch compare nkpl/fig11/*-first*
