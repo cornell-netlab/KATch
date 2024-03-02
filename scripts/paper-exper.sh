48701c9befc638995e668bc875bd3f29ce62812e
Mark Moeller <moeller@cs.cornell.edu>
2024-03-02 08:13:58 -0500

    update scripts

--- scripts/paper-exper.sh
+++ scripts/paper-exper.sh
@@ -2,4 +2,6 @@
 ./katch compare 300s nkpl/fig09/naive-reachability
 ./katch run nkpl/fig09/linear-reachability
 ./katch compare 48h nkpl/fig10
-./katch compare 48h nkpl/fig11/*-first*
+./katch run nkpl/flip-first11
+./katch run nkpl/inc-first10
+./katch run nkpl/nondet-first15
