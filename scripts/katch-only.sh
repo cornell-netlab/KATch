477958ac6a458767dd3d21dae2e0fafa22f03b85
Mark Moeller <moeller@cs.cornell.edu>
2024-03-05 13:15:52 -0500

    scripts

--- scripts/katch-only.sh
+++ scripts/katch-only.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./katch run nkpl/fig09/naive-reachability
-./katch run nkpl/fig09/linear-reachability
-./katch run nkpl/fig10
-./katch run nkpl/fig11/flip-first11
-./katch run nkpl/fig11/inc-first10
+./katch run nkpl/fig09/naive-reachability && \
+./katch run nkpl/fig09/linear-reachability && \
+./katch run nkpl/fig10 && \
+./katch run nkpl/fig11/flip-first11 && \
+./katch run nkpl/fig11/inc-first10 && \
 ./katch run nkpl/fig11/nondet-first15
--- scripts/paper-exper.sh
+++ scripts/paper-exper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./katch compare 300s nkpl/fig09/naive-reachability
-./katch run nkpl/fig09/linear-reachability
-./katch compare 48h nkpl/fig10
-./katch compare 48h nkpl/fig11/flip-first11
-./katch compare 48h nkpl/fig11/inc-first10
+./katch compare 300s nkpl/fig09/naive-reachability && \
+./katch run nkpl/fig09/linear-reachability && \
+./katch compare 48h nkpl/fig10 && \
+./katch compare 48h nkpl/fig11/flip-first11 && \
+./katch compare 48h nkpl/fig11/inc-first10 && \
 ./katch compare 48h nkpl/fig11/nondet-first15
