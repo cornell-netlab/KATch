5b4344b40325e60e557dfc0d57e3e13a149fff51
Mark Moeller <moeller@cs.cornell.edu>
2024-03-09 19:18:53 -0500

    abridged script

---
+++ scripts/abridged.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in nkpl/fig09/naive-reachability/*; do ./katch compare 60s $i; done && \
+./katch run nkpl/fig09/linear-reachability && \
+./katch compare 48h nkpl/fig10-less-cogentco && \
+./katch compare 48h nkpl/fig11/flip-first11 && \
+./katch compare 48h nkpl/fig11/inc-first10 && \
+./katch compare 48h nkpl/fig11/nondet-first15
