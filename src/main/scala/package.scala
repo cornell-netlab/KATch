ab821221bd1efead60a9ece01381b80762692d98
Mark Moeller <moeller@cs.cornell.edu>
2024-03-29 13:53:47 -0400

    package level doc

---
+++ src/main/scala/package.scala
@@ -0,0 +1,7 @@
+/** The `nkpl` package comprises all of the code for reading NetKAT Programming Language (NKPL)
+ *  programs for checking equivalence of NetKAT programs via automaton
+ *  bisimulation. For details on the algorithms and datastructures used in the
+ *  implementation of the package, please consult our PLDI 2024 paper, "Katch:
+ *  A Fast Symbolic Verifier for NetKAT".
+ */
+package object nkpl
